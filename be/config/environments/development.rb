c658a165cfe7b32dec8e9d951712104d8b701aba
manhnguyen <manhnguyen230598@gmail.com>
2026-05-03 23:57:36 +0700

    Run background jobs with dedicated Solid Queue worker

--- be/config/environments/development.rb
+++ be/config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # config.generators.apply_rubocop_autocorrect_after_generate!
 
 
-  config.active_job.queue_adapter = :async
+  config.active_job.queue_adapter = :solid_queue
 
   config.action_cable.allowed_request_origins = [
     "http://localhost:3000",
