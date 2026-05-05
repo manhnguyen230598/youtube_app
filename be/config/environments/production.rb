7e5110caab751f336568bae1c98e3c357e495a31
manhnd <manhnd@ngvgroup.vn>
2026-05-05 15:47:04 +0700

    Add Render deployment pipeline

--- be/config/environments/production.rb
+++ be/config/environments/production.rb
@@ -84,4 +84,20 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.active_job.queue_adapter = :sidekiq
+
+  config.cache_store = :redis_cache_store, {
+    url: ENV.fetch("REDIS_URL"),
+    connect_timeout: 2,
+    read_timeout: 1,
+    write_timeout: 1
+  }
+
+  config.action_cable.url = ENV.fetch("ACTION_CABLE_URL")
+  config.action_cable.allowed_request_origins = [
+    ENV.fetch("FRONTEND_URL"),
+    /https:\/\/.*\.onrender\.com/
+  ]
+
+  config.hosts.clear
 end
